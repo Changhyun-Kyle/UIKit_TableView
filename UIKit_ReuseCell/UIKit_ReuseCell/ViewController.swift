53d0e16f4fc9d07445aa65d5b8e0491ba5178073
Changhyun Kang <changhyun.kyle@gmail.com>
2024-01-08 15:24:41 +0900

    feat: UITableView 구현

--- UIKit_ReuseCell/UIKit_ReuseCell/ViewController.swift
+++ UIKit_ReuseCell/UIKit_ReuseCell/ViewController.swift
@@ -9,11 +9,58 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    func address(of object: UnsafeRawPointer) -> String{
+        let address = Int(bitPattern: object)
+        return String(format: "%p", address)
+    }
 }
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard
+            let cell = tableView.dequeueReusableCell(withIdentifier: CustomTableViewCell.identifier) as? CustomTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        cell.setUpCell(index: indexPath.row)
+        
+        if indexPath.row == 0 {
+            cell.backgroundColor = .red
+        }
+        var addressCell = cell
+        print("cellForRowAt - \(indexPath.section), \(indexPath.row), \(address(of: &addressCell))")
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        var cell = cell
+        print("UITableView : willDisplay - \(indexPath.section), \(indexPath.row), \(address(of: &cell))")
+    }
+    
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        var cell = cell
+        print("UITableView : didEndDisplaying - \(indexPath.section), \(indexPath.row), \(address(of: &cell))")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
 
